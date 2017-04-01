faf054271ab7357455986dc23dc4ac72dc84b718
kaijento <kaijento@users.noreply.github.com>
2017-04-01 15:14:58 +0100

    scrapy example

---
+++ webscraping/steamcommunity.com/get-group-members.py
@@ -0,0 +1,28 @@
+'''
+author: Karl Thornton <karl.genockey.thornton@gmail.com
+https://kaijento.github.io/2017/04/01/web-scraping-steamcommunity.com/
+'''
+
+import scrapy
+
+group = 'KeyVendorNet'
+
+class SteamCommunity(scrapy.Spider):
+    name = 'Steam Community'
+
+    base_url = 'http://steamcommunity.com/groups/{}/members/'.format(group)
+    base_url += '?p={}'
+
+    start_urls = [
+        base_url.format(1)
+    ]
+
+    def parse(self, response):
+        last_page = response.css('.pagelink::text').extract()[-1]
+        for n in range(1, int(last_page) + 1):
+            yield scrapy.Request(
+                self.base_url.format(n), callback=self.extract_members)
+
+    def extract_members(self, response):
+        for href in response.css('.linkFriend::attr(href)'):
+            yield { 'href': href.extract() }
